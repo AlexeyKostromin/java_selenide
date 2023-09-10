8572d3f48be5b9455e29cb0999ab2fbddc770f34
Alexey Kostromin <kostromin.aleksei@gmail.com>
2023-09-10 18:56:22 +0200

    __Hello world update

--- src/main/java/com/alexey_k/Main.java
+++ src/main/java/com/alexey_k/Main.java
@@ -2,6 +2,8 @@ package com.alexey_k;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("__Hello world!");
+        // test1
     }
 }
